7fd209adf92540058c9a0c0ea078560720439cea
Neal Chris <tmbarnhart1@gmail.com>
2024-01-11 20:36:24 -0500

    Initialize Database: Drop and Create tech_blog_db

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+-- Drop and create database
+DROP DATABASE IF EXISTS tech_blog_db; -- Drop the database if it already exists to start with a clean slate
+CREATE DATABASE tech_blog_db; -- Create a new database named tech_blog_db
